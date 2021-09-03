c5ff21791e92537bf2fafb757bd47f371ce8ffae
csmcclain <christopher.mcclain@icloud.com>
2021-09-03 16:50:45 -0600

    Added Change to test trigger.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -55,6 +55,8 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
+        
+        System.out.println("Hello From intToBinaryNumber");
 
         StringBuilder stringBuilder = new StringBuilder();
 
