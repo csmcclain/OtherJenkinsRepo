5a1aab6050aed9bbb97451e7ca6a7b143321666e
csmcclain <christopher.mcclain@icloud.com>
2021-09-03 16:20:51 -0600

    Added Added Int to Binary.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -54,7 +54,15 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+         do {
+            stringBuilder.append(n % 2);
+            n = n / 2;
+        } while (n > 0);
+
+        return stringBuilder.reverse().toString();
     }
 
     /*
